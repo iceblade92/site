5525fe48550eefe27a2a88115dc9f9f710bb5755
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2025-11-15 15:01:50 +0100

    added text to html node and unit tests for empty link/url

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +23,55 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.TEXT)
         node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertEqual(node, node2)
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_code(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_link(self):
+        node = TextNode("This is a text node", TextType.LINK, "https://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.props, {"href": "https://example.com"})
+
+    def test_text_image(self):
+        node = TextNode("alt text", TextType.IMAGE, "https://img.example/x.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "https://img.example/x.png", "alt": "alt text"})
+
+class TestTextNodeURLValidation(unittest.TestCase):
+    def test_link_missing_url_raises(self):
+        node = TextNode("Click me", TextType.LINK, None)
+        with self.assertRaisesRegex(ValueError, "LINK requires a non-empty url"):
+            text_node_to_html_node(node)
+
+    def test_image_missing_url_raises(self):
+        node = TextNode("alt text", TextType.IMAGE, "")
+        with self.assertRaisesRegex(ValueError, "IMAGE requires a non-empty url"):
+            text_node_to_html_node(node)
 
 if __name__ == "__main__":
     unittest.main()
