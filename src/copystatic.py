1a25c0ba83f70289956fda3b95ff67bbdd92e5ec
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2025-11-24 17:32:45 +0100

    made functions copy file and dir from static to public

---
+++ src/copystatic.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+
+
+def delete_dir():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for name in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, name)
+        dest_path = os.path.join(dest_dir_path, name)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path) 
+
--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from textnode import TextNode, TextType
+from copystatic import delete_dir, copy_files_recursive
 
 def main():
     node = TextNode("hello world", TextType.TEXT)
     print(node)
+    delete_dir()
+    copy_files_recursive("static", "public")
 
 if __name__ == "__main__":
     main()
