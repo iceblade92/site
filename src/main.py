d81dc0ba828103792e5c47ac7afc943ad103bed6
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2025-11-25 15:20:28 +0100

    added template and content for generate content file and local host start

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
-from textnode import TextNode, TextType
 from copystatic import delete_dir, copy_files_recursive
+from gencontent import generate_page
 
 def main():
-    node = TextNode("hello world", TextType.TEXT)
-    print(node)
     delete_dir()
     copy_files_recursive("static", "public")
+    from_path = "./content/index.md"
+    template_path = "./template.html"
+    dest_path = "./public/index.html"
+    generate_page(from_path, template_path, dest_path)
 
 if __name__ == "__main__":
     main()
