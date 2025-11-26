92b25b6c17b54c6c0f33f3509a56ee61fb382c81
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2025-11-26 14:34:41 +0100

    fixed recursive function after i deleted part of it

--- src/gencontent.py
+++ src/gencontent.py
@@ -21,3 +21,21 @@ def extract_title(markdown):
             title = line[2:].strip()
             return title
     raise Exception("No h1 header found")
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as f:
+        markdown_content_from = f.read()
+    with open(template_path, "r") as f:
+        markdown_content_template = f.read()
+    node = markdown_to_html_node(markdown_content_from)
+    html_content = node.to_html()
+    title = extract_title(markdown_content_from)
+    template = markdown_content_template
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html_content)
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir != "":
+        os.makedirs(dest_dir, exist_ok=True)
+    with open(dest_path, "w") as f:
+        f.write(template)
